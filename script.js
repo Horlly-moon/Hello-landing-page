c6e11f651f485bc24ac15a61d5aafca4b43b22c5
Horlly-moon <moonhorlly@gmail.com>
2023-09-05 16:27:44 +0100

    Updated files

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.querySelector(".ham-menu").addEventListener("click", function () {
     this.classList.toggle("open");
+    document.querySelector(".toggle-text").classList.toggle("hidden");
 });
